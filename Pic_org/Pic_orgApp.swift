b3969bedf37d27068aec280424d7c37ef471c904
drshivago <moessner2015@gmail.com>
2022-07-15 22:25:54 -0400

    Project Start

---
+++ Pic_org/Pic_orgApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pic_orgApp.swift
+//  Pic_org
+//
+//  Created by Steven on 7/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Pic_orgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
